30dac6d9ec039cd98f3f1565173c6bdd064fc5d0
linglu <lusoul00@gmail.com>
2017-09-03 00:15:53 -0700

    Test

--- src/com/caden/Test.java
+++ src/com/caden/Test.java
@@ -2,5 +2,5 @@ public class Test {
 	public static void main(String[] args) {;
 	}
 }
-
-I'm in the second branch, this is the message I add in second branch
+what the fuck
+I'm  the message I add in second branch
